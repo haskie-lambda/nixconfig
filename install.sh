3edf312f0f4888b178f4f26fdf63756e0543d848
Fabian Schneider <SCFA@at.ema.ad.pwcinternal.com>
2020-09-17 12:04:44 +0100

    fixed sudo-only-access-to-nixhome userfolder

--- install.sh
+++ install.sh
@@ -37,6 +37,7 @@ rm /mnt/etc/nixos/configuration.nix
 yes | cp -rf ./nixos/* /mnt/etc/nixos/
 mkdir -p /mnt/home/faebl/.config/nixpkgs
 yes | cp -rf ./nixpkgs/* /mnt/home/faebl/.config/nixpkgs
+sudo chmod -R ugo+rw /mnt/home/faebl/.config/nixpkgs
 
 nixos-install
 #reboot
