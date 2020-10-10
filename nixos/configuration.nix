d4d9e819856dfaf64718de4fee6f62fa8ea9a439
faebl <faebl.taylor@protonmail.com>
2020-10-10 17:56:39 +0200

    cleaned v2

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,14 +4,6 @@
   imports = [ 
      ./hardware-configuration.nix
      ./v2/configuration.nix 
-   # Include the results of the hardware scan.
-#    ./hardware-configuration.nix
-#    ./devices/private_machine.nix
   ];
 
-#  programs.zsh.ohMyZsh = {
-#    enable = true;
-#    plugins = [ "git" "man" ];
-#    theme = "agnoster";
-#  };
 }
